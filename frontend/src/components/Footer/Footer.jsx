713da98747b389ae1a19677498cebec4deae1547
Muhammad Hafiz Hisbullah <hafichever@gmail.com>
2024-07-28 00:08:37 +0700

    feat(UI): add footer section

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <section className="footer bg-primary md:py-4">
+        <div className="content-footer flex flex-col text-white text-center">
+          <h3 className="font-bold">GreenTrail.id 2024</h3>
+          <p className="font-light">
+            Empowering Communities, Preserving Nature for All
+          </p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Footer;
