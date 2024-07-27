d4644f687c1c4fcf94b6b97524b55b1a6cc607fb
Muhammad Hafiz Hisbullah <hafichever@gmail.com>
2024-07-28 00:31:47 +0700

    fix(UI): footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <section className="footer bg-primary md:py-4">
-        <div className="content-footer flex flex-col text-white text-center">
-          <h3 className="font-bold">GreenTrail.id 2024</h3>
-          <p className="font-light">
+      <section className="footer bg-primary py-3 md:py-4">
+        <div className="content-footer w-11/12 mx-auto flex flex-col text-white text-center">
+          <h3 className="font-bold text-base md:text-lg">GreenTrail.id 2024</h3>
+          <p className="font-light text-sm md:text-base">
             Empowering Communities, Preserving Nature for All
           </p>
         </div>
